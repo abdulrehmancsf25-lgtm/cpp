c3eb767604ba2e0f4e68c796e6fa2c01aaf67c0e
abdulrehmancsf25-lgtm <abdulrehmanarshad680@gmail.com>
2025-12-19 22:17:33 +0500

    binary search for a given key in arrays

---
+++ lecture12/binsearch.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int search(int arr[] , int n , int key){
+    int start=0;
+    int end=n-1;
+    int mid=start +(end -start)/2 ;
+    while(start<=end){
+if(arr[mid]==key){
+    return mid;
+}
+if(arr[mid]<key){
+    start=mid+1;
+}
+if(arr[mid]>key){
+    end=mid-1;
+}
+mid=start +(end -start)/2 ;
+    }
+        return -1;
+}
+int main(){
+int app[5]={3,5,7,9,10};
+int qpp[10]={323,345,356,367,377,390,400,600 ,794,999};
+int ans=search(qpp,10,999);
+cout << " the index of given key is "<<ans << endl;
+    return 0;
+}
