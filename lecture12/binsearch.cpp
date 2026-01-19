b49b8de8e99c1377b3b44d97828e18a3c643a785
abdulrehmancsf25-lgtm <abdulrehmanarshad680@gmail.com>
2026-01-19 15:09:18 +0500

    STL ARRAY

--- lecture12/binsearch.cpp
+++ lecture12/binsearch.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include<vector>
+#include<utility>
 using namespace std;
-int search(int arr[] , int n , int key){
+/*int search(int arr[] , int n , int key){
     int start=0;
     int end=n-1;
     int mid=start +(end -start)/2 ;
@@ -22,6 +24,19 @@ int main(){
 int app[5]={3,5,7,9,10};
 int qpp[10]={323,345,356,367,377,390,400,600 ,794,999};
 int ans=search(qpp,10,999);
-cout << " the index of given key is "<<ans << endl;
+cout << " the index of given key is "<<ans << endl;*/
+int main(){
+
+            pair <int,int> p;
+            p.first=1;
+            p.second=4;
+            cout<<p.first << p.second <<endl;
+            vector<int> app;
+            app.push_back(12);
+            app.push_back(3);
+            app.push_back(64);
+            cout << app[2];
+
+
     return 0;
 }
---
+++ lecture12/stlvector.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std ;
+int main(){
+    /*
+       vector<int> v ;
+cout <<" capacity "<< v.capacity() << endl ;
+cout <<"size " << v.size() << endl ;
+v.push_back(9);
+
+v.push_back(4);
+cout <<"capacity"<<  v.capacity() << endl ;
+cout << v.at(0) << endl ;
+
+v.push_back(3) ;
+cout << " capacity" << v.capacity() << endl ;
+cout << v.at(1) << endl  ;
+v.push_back(7) ;
+cout <<"capacity"<< v.capacity() << endl;
+
+v.insert(v.begin() , {3,4,5});
+for (int i = 0 ; i < v.size() ; i++){
+    cout << v[i] << " " ;
+}
+cout << endl ;
+cout << v.front() << endl;
+// to remove an element from vector
+v.pop_back();
+for (int i = 0 ; i < v.size() ; i++){
+    cout << v[i] << " " ;
+}
+     */ 
+           vector<int> arr(5,3);
+           for(int i = 0 ; i < arr.size() ;i++ ){
+            cout << arr[i] << " " ;
+           }
+           cout << endl;
+vector<int> last(arr) ;
+ for(int i = 0 ; i < last.size() ;i++ ){
+            cout << last[i] << " " ;
+           }
+
+
+    return 0 ;
+}
