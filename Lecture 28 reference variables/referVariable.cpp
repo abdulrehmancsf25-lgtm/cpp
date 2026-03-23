dfb8b9c711379c21065eb3ad00f540e4079a830e
DESKTOP-SJO43KC\Lenovo <abdulrehmancsf25@gmail.com>
2026-03-22 22:47:23 -0700

    Reference variables and pass by refernce

--- Lecture 28 reference variables/referVariable.cpp
+++ Lecture 28 reference variables/referVariable.cpp
@@ -14,6 +14,19 @@ using namespace std ;
 void update1(int& n ){
     n++ ;
 }
+// 2 -->  
+           // RETURN A COPY (PASS BY VALUE)
+        int getElement1(int arr[] , int i){
+                 return arr[i] ;
+        }
+         
+     int& getElement2(int arr[] , int index){
+        return arr[index] ;
+     }
+
+
+
+
 int main(){
     /*
   int  i = 5 ;
@@ -26,5 +39,18 @@ int main(){
  cout << "Before value " <<  n << endl ;
   update1(n) ;
   cout << "After value " << n << endl ;
+
+     // REFERENCES AS RETURN VARIABLES
+
+     int arr[5] = {0,1,2,3,4} ;
+    // CALLING GETELEMENT1 FUNCTION (PASS BY VALUE)
+    //  getElement1(arr , 2) = 3 ;  (Not possible to set value)
+    int x = getElement1(arr , 2) ;
+    cout << "Printing x : " << x << endl ;
+
+    // CALLING GETELEMENT1 FUNCTION (PASS BY REFERENCE)
+   getElement2(arr , 2) = 100 ;
+   cout << "PRINTING NEW SET VALUE  :  "  << getElement2(arr , 2) << endl ;
+   cout <<"Printing again :  "<<  arr[2] << endl ;
     return 0 ;
 }
