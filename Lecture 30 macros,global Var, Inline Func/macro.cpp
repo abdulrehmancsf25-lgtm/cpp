3f0eb18cba34bfd620ae7bb749ac600a8bbf1b00
DESKTOP-SJO43KC\Lenovo <abdulrehmancsf25@gmail.com>
2026-03-24 16:53:20 -0700

    Macros

---
+++ Lecture 30 macros,global Var, Inline Func/macro.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#define pi 3.14
+using namespace std ;
+int main(){
+    int r = 5  ;
+    cout << "Area of circle is " << 3.14 * r * r << endl ;
+    cout << "Circumference is " << 3.14 * r * r << endl ;
+
+    cout << "Area of circle is " << pi * r * r << endl ;
+    cout << "Circumference is " << pi * r * r << endl ;
+    return 0 ;
+}
