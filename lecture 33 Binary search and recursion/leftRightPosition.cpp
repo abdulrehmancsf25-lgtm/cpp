0dea0c23d5157e964948efa38897bd7c3d903a0a
DESKTOP-SJO43KC\Lenovo <abdulrehmancsf25@gmail.com>
2026-04-23 10:26:01 -0700

    Finding peak element of array using binary search and recursion

--- lecture 33 Binary search and recursion/leftRightPosition.cpp
+++ lecture 33 Binary search and recursion/leftRightPosition.cpp
@@ -20,7 +20,7 @@ int left_Position(int* arr , int s , int e ,int target ,int index){
 }
 int main(){
     int arr[] = {2,4,6,8,10,12} ;
-    int end = sizeof(arr) /2 -1  , index = -1 ;
+    int end = sizeof(arr) /4 -1  , index = -1 ;
     int ans = left_Position(arr , 0 , end ,12 ,index) ;
    cout << "left most occurrence of given target is at index :  "  << ans ;
     return 0 ;
---
+++ lecture 33 Binary search and recursion/peakElement.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std ;
+int peak_Element(int* arr , int s , int e ){
+   
+    // base case
+    if(s >= e)
+    return arr[s] ;
+
+    int mid = s + (e-s)/2 ;
+   
+     if ( arr[mid] < arr[mid+1])
+    return peak_Element( arr , mid +1 , e ) ;
+
+    else
+    return peak_Element( arr , s , mid  ) ;
+   
+}
+int main(){
+    int arr[] = {2,4,6,8,7,4,2,1} ;
+    int end = sizeof(arr) /4 -1 ;
+    peak_Element(arr , 0 , end ) ;
+   cout << "peak element of array is  :  "  <<  peak_Element(arr , 0 , end ) ;
+    return 0 ;
+}
