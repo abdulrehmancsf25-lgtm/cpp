1459f039051f8c12f95968b1b64dde0900edb6ae
DESKTOP-SJO43KC\Lenovo <abdulrehmancsf25@gmail.com>
2026-04-23 09:24:24 -0700

    Find  left most occurrence of element in sorted array using recursion

---
+++ lecture 33 Binary search and recursion/leftRightPosition.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std ;
+int left_Position(int* arr , int s , int e ,int target ,int index){
+   
+    // base case
+    if(s > e)
+    return index ;
+
+    int mid = s + (e-s)/2 ;
+    if(arr[mid] == target ){
+        index = mid ;
+        return left_Position( arr , s , mid -1 ,target , index) ;
+    }
+    else if ( arr[mid] > target)
+    return left_Position( arr , s , mid -1 ,target , index) ;
+
+    else
+    return left_Position( arr , mid +1 , e ,target , index) ;
+   
+}
+int main(){
+    int arr[] = {2,4,6,8,10,12} ;
+    int end = sizeof(arr) /2 -1  , index = -1 ;
+    int ans = left_Position(arr , 0 , end ,12 ,index) ;
+   cout << "left most occurrence of given target is at index :  "  << ans ;
+    return 0 ;
+}
