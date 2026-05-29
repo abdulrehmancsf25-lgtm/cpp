8aac9cb882215be049df6bf48848012b55dee7c0
DESKTOP-SJO43KC\Lenovo <abdulrehmancsf25@gmail.com>
2026-05-29 12:11:15 +0500

    Tic Tac toe singlr & double player

--- Projects/ticTac.cpp
+++ Projects/ticTac.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #define mid 1 
 using namespace std;
@@ -30,6 +31,25 @@ void drawBoard(char board[3][3]) {
         }
     }
 }
+void computer_pl(char arr[3][3], bool check[9]) {
+    // Take random column and row number to place input 
+    int rowNo, colNo;
+    bool place = true;
+    while(place) {
+        rowNo = rand() % 3;
+        colNo = rand() % 3;
+        // checking 1d boolean array for the availability of place 
+        if(check[3 * rowNo + colNo] != true) {
+            // placing computer input
+            arr[rowNo][colNo] = 'O';
+            check[3 * rowNo + colNo] = true;
+            place = false;
+        }
+    }
+        long long delay = 100000000;
+        while(delay--);
+    
+}
 bool check(char arr[][3] , int rows , char playerSign){
           // row check 
            for (int i = 0; i <  rows; i++) {
@@ -45,24 +65,33 @@ bool check(char arr[][3] , int rows , char playerSign){
             
           }  
         // Main diagonal
+        if(arr[mid][mid] == playerSign) {
         if(arr[mid-1][mid-1] == arr[mid][mid] && arr[mid][mid] == arr[mid+1][mid+1])
          return true ;
         
         // secondary diagonal
         if(arr[mid-1][mid+1] == arr[mid][mid] && arr[mid][mid] == arr[mid+1][mid-1])
           return true ;
+        }
     // else
      return false ;
 }
-void start_Match(){
+void start_Match( ){
                       char player = 'X', arr[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
     drawBoard(arr); // drawing board 
-    
+    bool single_Pl ;
+                        cout << "Press 1 for single player "<< endl ;
+                        cout << "Press 2 for multiplayer " << endl ;
+                        cin >> single_Pl ;
     int *count = new int(1), *input = new int; 
     bool temp[9] = {0};
 
     while(*count <= 9) {
+        
         cout << "Player " << player << " turn " << endl;
+        if(single_Pl && player == 'O')
+         computer_pl(arr ,temp) ;
+        else {
         cin >> *input;
 
       
@@ -74,6 +103,12 @@ void start_Match(){
         if(temp[*input - 1] == false) {
             arr[(*input - 1) / 3][(*input - 1) % 3] = player;
             temp[*input - 1] = true; 
+        }
+        else {
+            cout << " Invalid place " << endl;
+            continue; 
+        }
+    }
             // for delay between inputs 
              long long  delay = 100000000;
             while(delay--) ;
@@ -83,6 +118,8 @@ void start_Match(){
             if(*count >= 5) {
                 if(check(arr, 3, player)) {
                     display_Win(player) ;
+                     delete count;
+                     delete input;
                     return ;
                 }
             }
@@ -93,21 +130,17 @@ void start_Match(){
                 player = 'O';
             else
                 player = 'X';
-        }
-        else {
-            cout << " Invalid place " << endl;
-        }
-
-        if(*count > 9) { // count  to > 9 to handle draw 
+                if(*count > 9) { // count  to > 9 to handle draw 
             display_Draw() ;
             break;
         }
-    }
-
+        }
+        
     delete count;
     delete input;
 }
 int main() {
+            srand(time(0)) ;
                char  process  ;
               while(1){
                         game_Start_Display() ;
